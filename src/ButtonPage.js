5b3a438c28f793b86816da34f0f2ca548767c12a
Lucas Shanker <lshanke@purdue.edu>
2017-08-12 15:48:37 -0500

    Style for point counter and give button

--- src/ButtonPage.js
+++ src/ButtonPage.js
@@ -8,13 +8,20 @@ class ButtonPage extends Component{
 
     render(){
         return(
-            <div className = 'buttonPage'>
-                <button className = "bigButton" onClick = {this.props.incrementPoints}><p>Click Here!</p></button>
-                <p className = "pointCount">Points: {this.props.possessions.points}</p>
-                <p className = "pointCount">Your Score: {this.props.possessions.given}______</p>
-                <p className = "pointCount">Team Points: {this.props.colors[this.props.possessions.color]}______</p>
-                <button className = "giveButton" onClick = {() => {this.props.incrementTeam(this.props.possessions.color, this.props.possessions.points)}}>Give points to team</button>
-            </div>
+            <div className = "buttonPage">
+                <div className = 'buttonContainer'>
+                    <button className = "bigButton" onClick = {this.props.incrementPoints}></button>
+                </div>
+                <div>
+                    <div className = "pointContainer">
+                        <p className = "pointCount">Points: {this.props.possessions.points}</p>
+                        <button className = "giveButton" onClick = {() => {this.props.incrementTeam(this.props.possessions.color, this.props.possessions.points)}}>Give points to team <i className="fa fa-hand-o-right" aria-hidden="true"></i></button>
+                    </div>
+                    <p className = "score">Your Score: {this.props.possessions.given}______</p>
+                    <p className = "teamCount">Team Points: {this.props.colors[this.props.possessions.color]}______</p>
+                </div>
+                
+           </div>
         )
     }
 
