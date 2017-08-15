c7d7e6219324b8e72dc60e884b3736e1d592522e
Lucas Shanker <lshanke@purdue.edu>
2017-08-15 18:38:34 -0500

    Check and update deadline in server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
 var admin = require("firebase-admin");
+var http = require('http')
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+// Fetch the service account key JSON file contents
+var serviceAccount = require("./color-clicker-b234a-firebase-adminsdk-ka48o-41f5d06651.json");
 
+// Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
+  databaseURL: "https://color-clicker-b234a.firebaseio.com"
 });
+
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("deadline");
+
+var interval = setInterval(() => {
+    ref.once("value", function(snapshot) {
+     console.log(snapshot.val());
+     if(Date.now()>=snapshot.val()){
+         console.log('time is up!')
+         ref.set(Date.now() + 3600000)
+     }
+    });
+}, 1000)
